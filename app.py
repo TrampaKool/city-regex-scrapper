708776ca285cf58d6a606d33ee436fc8273f087a
TrampaKool <labismail25@gmail.com>
2026-03-19 11:12:33 +0200

    add average temperature text detection

--- app.py
+++ app.py
@@ -17,9 +17,37 @@ def scrape_city_info(url):
         return "infobox not found"
     
 
-    text = infobox.get_text(separator=" ", strip=True)
+    infobox_text = infobox.get_text(separator=" ", strip=True)
 
-    # regex
+    # find temperature table
+    average_temperature_text = None
+    tables = soup.find_all('table', {'class': 'wikitable'})
+
+    climate_table = None
+
+    for table in tables:
+        if "Κλιματικά δεδομένα" in table.get_text():
+            climate_table = table
+            break
+
+    if climate_table:
+
+        rows = climate_table.find_all('tr')
+
+        for row in rows:
+            header_cell = row.find('th')
+
+            if header_cell and "Μέση Μηνιαία" in header_cell.get_text():
+                row_cells = row.find_all('td')
+
+                last_cell = row_cells[-1] if row_cells else None
+                if last_cell:
+                    average_temperature_text = last_cell.get_text(strip=True)
+    else:
+        print("climate table not found.")
+
+    
+
     patterns = {
         "infobox_text": {
             "Πληθυσμός": r"(?:Πληθυσμός|Population)\s*([\d\.,\s]+)",
@@ -40,9 +68,9 @@ def scrape_city_info(url):
     }
 
     results = {}
-    for key, pattern in patterns.items():
+    for key, pattern in infobox_patterns.items():
         match = re.search(pattern, text)
-        results[key] = match.group(1).strip() if match else "Δεν βρέθηκε"
+        results[key] = match.group(1).strip() if match else "Not found"
 
     return results
 
